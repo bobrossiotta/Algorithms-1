2d3af1934db219b0667ab30c58320d1f9ab0d614
bobrossiotta <137832556+bobrossiotta@users.noreply.github.com>
2023-12-18 03:09:07 +0300

    Add files via upload

---
+++ README.java
@@ -0,0 +1,32 @@
+/* 
+Proje 1: Insertion Sort
+
+Verilen dizi: [22, 27, 16, 2, 18, 6]
+
+Adım: [2, 22, 27, 16, 18, 6]
+Adım: [2, 6, 22, 27, 16, 18]
+Adım: [2, 6, 16, 22, 27, 18]
+Adım: [2, 6, 16, 18, 22, 27]
+Adım: [2, 6, 16, 18, 22, 27]
+Adım: [2, 6, 16, 18, 22, 27]
+Big-O Gösterimi:
+Insertion Sort'un Big-O gösterimi 
+O(n²) dir.
+
+Time Complexity:
+Dizi sıralandıktan sonra 18 sayısı, Average case kapsamına girer. Çünkü Average case, aranan elemanın dizinin ortasında olması durumudur.
+
+Average case: Aradığımız sayının ortada olması
+Worst case: Aradığımız sayının sonda olması
+Best case: Aradığımız sayının dizinin en başında olması
+Proje 1: Selection Sort
+
+Verilen dizi: [7, 3, 5, 8, 2, 9, 4, 15, 6]
+
+Adım: [2, 3, 5, 8, 7, 9, 4, 15, 6]
+Adım: [2, 3, 5, 8, 7, 9, 4, 15, 6]
+Adım: [2, 3, 4, 8, 7, 9, 5, 15, 6]
+Adım: [2, 3, 4, 5, 7, 9, 8, 15, 6]
+Bu şekilde devam eder. Her adımda minimum eleman seçilir ve sıralanan kısma eklenir.
+
+*/
